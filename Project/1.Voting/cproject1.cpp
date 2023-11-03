ae5e9bde2faa7dbe761f4a56810849e1089cd6e8
Vikesh <vikeshkapoor100@gmail.com>
2023-11-04 00:24:03 +0530

    Added authentication process

--- Project/1.Voting/cproject1.cpp
+++ Project/1.Voting/cproject1.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <stdlib.h>
 #include <string.h>
 using namespace std;
 void candidate_details();
 void result();
 int i = 0, n, x;
 int candidate1 = 0, candidate2 = 0, candidate3 = 0, nota = 0;
+class voting
+{
+private:
+    int n, wrong = 0;
+    string username = "vikesh";
+    string password = "123456789";
 
+public:
+    string un,pwd;
+    void authentication_process();
+};
 // Voter class
-class voter
+class voter : public voting
 {
     char name[20];
     int age, vote;
@@ -15,23 +26,28 @@ class voter
 public:
     void getdata();
     void voting();
-} v[100];
+};
 
 // list to vote or see results
 int main()
 {
+    voter v[100];
+    v[1].authentication_process();
     int i = 0;
     candidate_details();
     cout << endl
          << endl
          << "\t\t(---VOTING---)" << endl;
-    cout <<endl<< "Enter the no. to perform task";
+    cout << endl
+         << "Enter the no. to perform task";
     do
     {
         cout << endl
              << "1 - Voting" << endl
              << "2 - See results" << endl
-             << "3 - Exit" << endl;
+             <<"3 - Candidate details" <<endl
+             << "4 - Logout" << endl
+             <<"5 - Exit"<<endl;
         cout << "->";
         cin >> x;
         switch (x)
@@ -50,22 +66,79 @@ int main()
         }
         case 3:
         {
+            candidate_details();
             break;
+        }
+        case 4:
+        {
+            cout << endl
+                 << "Logged out" << endl;
+            v[1].authentication_process();
+            break;
+        }
+        case 5:
+        {
+            cout<<endl<<"Exiting system";
+            exit(0);
         }
         default:
         {
             cout << "Choose valid no." << endl;
         }
         }
-    } while (x != 3);
+    } while (x != 4 || x!=5);
+}
+
+// authentication process
+void voting::authentication_process()
+{
+    cout << endl
+         << "!!!!!!!!!!!!!!!!!!!!!!!!!!!-----Voting System-----!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+    cout << endl
+         << "Press 1 to login : ";
+    cin >> n;
+authentication:
+    if (n == 1)
+    {
+        cout << endl
+             << "Authentication process" << endl;
+        cout << endl
+             << "Enter admin user name : ";
+        cin >> un;
+        cout << "Enter password : ";
+        cin >> pwd;
+        if (un == username && pwd == password)
+        {
+            cout << "Logged in , Access granted" << endl;
+        }
+        else
+        {
+            cout << "Username or password is incorrect  ,Access denied";
+            wrong++;
+            cout <<endl<< 3 - wrong << " chances left" << endl;
+            if (wrong > 2)
+            {
+                cout <<"You cannot access this system as you have entered wrong details multiple times" <<endl<< endl;
+                exit(0);
+            }
+            else
+            {
+                goto authentication;
+            }
+        }
+    }
+    else
+    {
+        cout << "Enter valid no. ";
+    }
 }
 
 // Candidate details
 void candidate_details()
 {
-    cout<<"------------------------------------------------------------------------------------------"<<endl;
-    cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
-    cout<<"------------------------------------------------------------------------------------------"<<endl;
+    cout << "------------------------------------------------------------------------------------------" << endl;
+    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+    cout << "------------------------------------------------------------------------------------------" << endl;
     cout << "Candidate id no. - 1"
          << endl
          << "\t\tName - Vikesh"
@@ -87,10 +160,9 @@ void candidate_details()
          << "Id no. - 4"
          << endl
          << "\t\tNOTA" << endl;
-    cout<<"------------------------------------------------------------------------------------------"<<endl;
-    cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
-    cout<<"------------------------------------------------------------------------------------------"<<endl;
-
+    cout << "------------------------------------------------------------------------------------------" << endl;
+    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+    cout << "------------------------------------------------------------------------------------------" << endl;
 }
 
 // taking voter details
@@ -101,7 +173,6 @@ void voter::getdata()
     cout << "Enter your age = ";
     cin >> age;
 }
-
 // voting process
 void voter::voting()
 {
@@ -109,7 +180,8 @@ void voter::voting()
     {
         cout << "Enter candidate id number(voting) = ";
         cin >> vote;
-        cout<<"------------------------------------------------------------------------------------------";
+        cout<<"Vote added successful"<<endl;
+        cout << "------------------------------------------------------------------------------------------";
         if (vote == 1)
         {
             candidate1++;
@@ -130,7 +202,7 @@ void voter::voting()
     else
     {
         cout << "You are not eligible for voting (Because age should be >=18" << endl;
-        cout<<"------------------------------------------------------------------------------------------";
+        cout << "------------------------------------------------------------------------------------------";
     }
 }
 // results
@@ -138,9 +210,9 @@ void result()
 {
 // printing results
 results:
-    cout<<"------------------------------------------------------------------------------------------"<<endl;
-    cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
-    cout<<"------------------------------------------------------------------------------------------"<<endl;
+    cout << "------------------------------------------------------------------------------------------" << endl;
+    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+    cout << "------------------------------------------------------------------------------------------" << endl;
     cout << endl
          << "\t\t\tVoting results" << endl;
     cout << "Candidate 1 = " << candidate1 << endl
@@ -173,7 +245,7 @@ results:
              << "\tName = Darun" << endl
              << "\tNo. of votes = " << candidate3 << endl;
     }
-    cout<<"------------------------------------------------------------------------------------------"<<endl;
-    cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
-    cout<<"------------------------------------------------------------------------------------------"<<endl;
+    cout << "------------------------------------------------------------------------------------------" << endl;
+    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+    cout << "------------------------------------------------------------------------------------------" << endl;
 }
